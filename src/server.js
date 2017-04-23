9f23dfe24df80882979440da423b792b75a3f060
Jeffrey Chang <jeffreychang@Jeffreys-MacBook-Pro.local>
2017-04-23 00:44:07 -0400

    hapi

--- src/server.js
+++ src/server.js
@@ -1,2 +1,30 @@
+// Node packages
+import Hapi from 'hapi';
+
+// Personal files
 import mafia from './mafia';
-console.log(mafia(5));
+import config from './config/testing';
+
+const PORT = process.env.PORT || 8080;
+
+
+if (!(config)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
+
+const server = new Hapi.Server();
+server.connection({ port: PORT, host: 'localhost' });
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    reply("Hello");
+  }
+});
+
+server.start((err) => {
+  if (err) throw err;
+
+  console.log(`Server running at: ${server.info.uri}`);
+});
